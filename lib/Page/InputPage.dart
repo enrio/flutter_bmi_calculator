87b925de6e56657dce2bad4238158411c348f3d5
thucnv <nvthuc@outlook.com>
2019-09-25 22:07:13 +0700

    Finished create layout

--- lib/Page/InputPage.dart
+++ lib/Page/InputPage.dart
@@ -9,12 +9,68 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: AppViewCard(
+                        color: Color(0xff1d1f33),
+                      ),
+                    ),
+                    Expanded(
+                      child: AppViewCard(
+                        color: Color(0xff1d1f33),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: AppViewCard(
+                  color: Color(0xff1d1f33),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: AppViewCard(
+                        color: Color(0xff1d1f33),
+                      ),
+                    ),
+                    Expanded(
+                      child: AppViewCard(
+                        color: Color(0xff1d1f33),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class AppViewCard extends StatelessWidget {
+  // const AppViewCard({
+  //   Key key,
+  // }) : super(key: key);
+  final Color color;
+  AppViewCard({@required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: color, borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
