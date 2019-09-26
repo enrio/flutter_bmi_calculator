aa508d0c1633cbe54ae26d9487edf4e9fdca2702
thucnv <nvthuc@outlook.com>
2019-09-26 22:19:31 +0700

    Layout

--- lib/Page/InputPage.dart
+++ lib/Page/InputPage.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomContainerHeight = 80.0;
+const colorCard = Color(0xff1d1f33);
+const colorButton = Color(0xffea1556);
+
+const colorCardTitleActive = Color(0xfffeffff);
+const colorCardTitleInactive = Color(0xff8e8f99);
 
 class InputPage extends StatefulWidget {
   @override
@@ -20,12 +28,22 @@ class _InputPageState extends State<InputPage> {
                   children: <Widget>[
                     Expanded(
                       child: AppViewCard(
-                        color: Color(0xff1d1f33),
+                        color: colorCard,
+                        child: GenderIcon(
+                          color: colorCardTitleActive,
+                          icon: FontAwesomeIcons.mars,
+                          title: "Male",
+                        ),
                       ),
                     ),
                     Expanded(
                       child: AppViewCard(
-                        color: Color(0xff1d1f33),
+                        color: colorCard,
+                        child: GenderIcon(
+                          color: colorCardTitleInactive,
+                          icon: FontAwesomeIcons.venus,
+                          title: "Female",
+                        ),
                       ),
                     )
                   ],
@@ -33,7 +51,55 @@ class _InputPageState extends State<InputPage> {
               ),
               Expanded(
                 child: AppViewCard(
-                  color: Color(0xff1d1f33),
+                  color: colorCard,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Height",
+                        style: TextStyle(
+                            color: colorCardTitleInactive, fontSize: 18.0),
+                      ),
+                      SizedBox(
+                        height: 5.0,
+                        width: double.infinity,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            "180",
+                            style: TextStyle(
+                              fontSize: 56,
+                              color: colorCardTitleActive,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 25.0),
+                            child: Text(
+                              "cm",
+                              style: TextStyle(
+                                color: colorCardTitleInactive,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
+                        ],
+                      ), // 180cm
+                      SizedBox(height: 5.0),
+                      Slider(
+                        min: 0,
+                        max: 250,
+                        value: 180,
+                        onChanged: null,
+                        activeColor: colorButton,
+                        inactiveColor: colorCardTitleActive,
+                        divisions: 3,
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Expanded(
@@ -41,20 +107,124 @@ class _InputPageState extends State<InputPage> {
                   children: <Widget>[
                     Expanded(
                       child: AppViewCard(
-                        color: Color(0xff1d1f33),
+                        color: colorCard,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text("Weight",
+                                style: TextStyle(
+                                  color: colorCardTitleInactive,
+                                  fontSize: 18.0,
+                                )),
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            Text("74",
+                                style: TextStyle(
+                                  color: colorCardTitleActive,
+                                  fontSize: 56.0,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                IconButton(
+                                  icon: Icon(FontAwesomeIcons.minus),
+                                  onPressed: null,
+                                ),
+                                IconButton(
+                                  icon: Icon(FontAwesomeIcons.plus),
+                                  onPressed: null,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
                       ),
                     ),
                     Expanded(
                       child: AppViewCard(
-                        color: Color(0xff1d1f33),
+                        color: colorCard,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text("Age",
+                                style: TextStyle(
+                                  color: colorCardTitleInactive,
+                                  fontSize: 18.0,
+                                )),
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            Text("25",
+                                style: TextStyle(
+                                  color: colorCardTitleActive,
+                                  fontSize: 56.0,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                IconButton(
+                                  icon: Icon(FontAwesomeIcons.minus),
+                                  onPressed: null,
+                                ),
+                                IconButton(
+                                  icon: Icon(FontAwesomeIcons.plus),
+                                  onPressed: null,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
                       ),
                     )
                   ],
                 ),
               ),
+              Container(
+                color: colorButton,
+                margin: EdgeInsets.only(top: 10.0),
+                height: bottomContainerHeight,
+              ),
             ],
           ),
         ));
+  }
+}
+
+class GenderIcon extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String title;
+
+  GenderIcon({@required this.icon, @required this.title, @required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80.0,
+          color: color,
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          title,
+          style: TextStyle(color: color, fontSize: 18.0),
+        )
+      ],
+    );
   }
 }
 
@@ -63,11 +233,14 @@ class AppViewCard extends StatelessWidget {
   //   Key key,
   // }) : super(key: key);
   final Color color;
-  AppViewCard({@required this.color});
+  final Widget child;
+
+  AppViewCard({@required this.color, this.child});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: child,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
           color: color, borderRadius: BorderRadius.circular(10.0)),
